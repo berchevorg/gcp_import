d357a9aa1ee419a8768456ce90b98633d72f9ca8
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-03-15 08:40:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,22 @@ resource "google_cloud_run_service" "resource_tfc" {
     latest_revision = true
   }
 }
+
+
+resource "google_cloud_run_service" "resource_cli" {
+  name     = "cloudrun-srv1"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
